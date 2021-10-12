24338998f92108edc31fc8ede332f3ea9157827a
alotero <alejandropcj@gmail.com>
2021-10-12 18:21:44 -0500

    aaaa

---
+++ cli/src/components/HelloWorld.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div class="container">
+      <b-form @submit.prevent="buscarUsuario()" v-if="show">
+        <b-form-group
+          id="input-group-1"
+          label="Nombre Usuario:"
+          label-for="input-1"
+          description="Ingrese.1111"
+        >
+          <b-form-input 
+            id="input-1"
+            v-model="form.username"
+            type="text"
+            placeholder="Ingrese su1111"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label="password:" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="form.password"
+            type="password"
+            placeholder="Enter contraseña"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Submit</b-button>
+      </b-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      form: {
+        username: "",
+        password: "",
+      },
+      show: true,
+      users: []
+    };
+  },
+
+  props: {
+    msg: String,
+  },
+  methods: {
+    buscarUsuario() {
+      console.log("funciona buscar");
+      this.axios.get('users') .then(res=>{
+        
+        this.users = res.data;
+        console.log(this.users);
+        this.users.forEach((value, index) => {
+
+          console.log(value.username);
+          console.log(value.password);
+
+          if(this.form.username == value.username){
+
+            console.log("usuario existe");
+            if(this.form.password == value.password){
+
+              console.log("contraseña full")
+            }
+
+          }
+
+        });
+
+      })
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
+
+
+    <!--<p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul> -->
---
+++ cli/src/views/Menu.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+
+        <h1>Bienvenido a nuestro portal de citas</h1>
+    </div>
+</template>
+
+<script>
+export default {
+      name: 'Menu'
+}
+</script>
+
